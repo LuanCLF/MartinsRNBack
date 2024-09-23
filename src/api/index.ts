123a54c1c8d46311198710684e2cb318ca5cdf5f
Luan Charlys <firminocharlys@gmail.com>
2024-09-23 14:47:12 -0300

    feat: add cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,16 @@ import 'express-async-errors';
 import express from 'express';
 import routes from './routes';
 import errorMiddleware from './middleware/error';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
